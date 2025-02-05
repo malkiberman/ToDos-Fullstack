16404dbc522971fb1867845cf23f6b915cd3693a
malkiberman <559120b@gmail.com>
2025-02-05 17:55:38 +0200

    6

--- render-api-server/index.js
+++ render-api-server/index.js
@@ -1,6 +1,7 @@
 import renderApi from '@api/render-api';
-
-renderApi.auth('rnd_U9mdfqtBpDzk8g59xsSH0sSktwOZ');
+import dotenv from 'dotenv';
+dotenv.config();
+renderApi.auth(process.env.RENDER_API_KEY);
 renderApi.listServices({includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
