a67de9454352b4744b2bf37da3b114570468db58
malkiberman <559120b@gmail.com>
2025-02-05 11:38:31 +0200

    my commit message

---
+++ ToDoListReact-master/src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // שימוש ב-react-dom/client במקום react-dom
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // יצירת root חדש
+root.render(<App />);
---
+++ ToDoListReact-master/src/service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+export default {
+
+  getTasks: async () => {
+    console.log(process.env.REACT_APP_API_URL);
+    
+    const result = await axios.get(`/items`)    
+    return result.data;
+  },
+
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    //TODO
+    const result = await axios.post(`/`,{
+      Name:name,
+      IsComplete:false
+    })    
+    return {result};
+  },
+
+
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    console.log('setCompleted', { id, isComplete });
+    const result = await axios.put(`/items/${id}?iscomplete=${isComplete}`, {
+    });
+      
+    return {result};
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask')
+
+    const result = await axios.delete(`/items/${id}`, {
+    });
+
+  }
+};
