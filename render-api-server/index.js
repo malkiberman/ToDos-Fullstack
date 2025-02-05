d61fe40dd94af5d563bae4c0b173190092806d87
malkiberman <559120b@gmail.com>
2025-02-05 18:23:56 +0200

    8

--- render-api-server/index.js
+++ render-api-server/index.js
@@ -1,7 +1,22 @@
 import renderApi from '@api/render-api';
+import express from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config();
-renderApi.auth(process.env.RENDER_API_KEY);
+
+// אוטנטיקציה עם Render API
+renderApi.auth(process.env.RENDER_API_KEY); // הכנס את ה-API Key שלך
 renderApi.listServices({includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
+
+const app = express();
+const port = process.env.PORT || 3000; // Render נותן לך את הפורט דרך משתנה סביבה
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
